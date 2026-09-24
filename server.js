77f86d2f898831ace49fac0b8ba2685776d2bc0d
Gostev <m-shock@yandex.ru>
2026-09-24 21:56:48 +0300

    fix

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'index.html');
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
+      res.end('Ошибка загрузки index.html');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.end(content);
+  });
+});
+
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Сервер запущен!`);
+  console.log(`Откройте на телефоне: http://game.local:${PORT}`);
+});
